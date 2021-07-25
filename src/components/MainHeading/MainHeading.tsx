88b55612e7550771cd43c72e882dc44848664d9f
Debbie Cheng <dqrcheng@gmail.com>
2021-07-25 15:29:37 +1000

    Add routing; style changes

--- src/components/MainHeading/MainHeading.tsx
+++ src/components/MainHeading/MainHeading.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from 'react';
 import styles from './MainHeading.module.css';
+import Navbar from './components/Navbar/Navbar';
 
 const MainHeading: FC = () => (
   <header className={styles.headingWrapper}>
-    <h1 className={styles.headingTitle}>Deb.</h1>
+    <Navbar />
+    <h1 className={styles.headingTitle}>
+      {'Hi, I\'m Deb.'}
+    </h1>
     <div className={styles.subGroup}>
       <h2 className={styles.subTitle}>Frontend Dev </h2>
       <span className={styles.divider}>\\</span>
